03b067c120adff5a858fea9ba6744b006ceee1d5
Diego <lucy@lucy.local>
2023-04-12 11:18:02 -0600

    implemented a get_auth_type method that allows user to specify the type of authentication their network has. No guarantees that the open, wep, and wpa works in a qr code. Note: wpa is depricated

--- wifi_qr_generator/wifi_qr_generator.py
+++ wifi_qr_generator/wifi_qr_generator.py
@@ -7,17 +7,21 @@ import pathlib
 
 class QR_Code_Generator:
   def generate():
-    # Get the SSID and password from the user
+    # Get the SSID from user
     ssid = input("SSID: ")
+    # Get the password from the user
     # password = getpass.getpass("Password: ")
     password = input("Password: ")
+    # get the auth type from user input
+    auth_type = QR_Code_Generator.get_auth_type(input("WPA/WPA2: "))
     
     # Create a progress bar to show the status of the QR code generation
     progress_bar = tqdm(desc="Generating QR code", total=1)
 
-    # Generate the QR code data
-    qr_data = f"WIFI:T:WPA2;S:{ssid};P:{password};;"
-  
+    # Generate the QR code data string
+    qr_data = f"WIFI:T:{auth_type};S:{ssid};P:{password};;"
+    print(qr_data)
+    
     # convert the ssid to snake_case for the output file
     file_name = QR_Code_Generator.create_file_name(ssid)
     
@@ -39,6 +43,23 @@ class QR_Code_Generator:
   # takes a string and returns a snake_case version of it "Example Ssid" -> "example_ssid
   def create_file_name(ssid) -> str:
     return ssid.replace(" ", "_").lower() + "_wifi_qr_code.png"
+  
+  # Takes a string from user input and returns the auth type. If empty, returns WPA2 as a default
+  def get_auth_type(auth_type: str) -> str:
+      auth_type = auth_type.strip().upper()
+      auth_types = {
+          "": "WPA2",
+          "WEP": "WEP",
+          "WPA": "WPA",
+          "WPA2": "WPA2",
+          "WPA3": "WPA3",
+          "OPEN": "Open"
+      }
+      if auth_type in auth_types:
+          return auth_types[auth_type]
+      else:
+          raise ValueError("Invalid authentication type")
+
 
 if __name__ == "__main__":
   qr = QR_Code_Generator
